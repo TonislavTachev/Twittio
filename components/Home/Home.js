fd04839bc2b1243beb40ff3db78bc1e7d457cca6
Tonislav Tachev <tachevv_@abv.bg>
2020-03-08 13:58:31 +0200

    added the home screen

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,6 +1,12 @@
-import React from 'react'
+import React, {useContext} from 'react'
 import {View, StyleSheet, Text} from 'react-native'
+import AuthContext from '../../context/AuthContext/authContext';
 const Home = () => {
+
+  const authContext = useContext(AuthContext);
+  const {user} = authContext;
+
+
     return (
         <View>
             
