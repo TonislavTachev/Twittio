b856f8cbf91b0433e3fd7d46d6bcaae14a4ebdf7
Tonislav Tachev <tachevv_@abv.bg>
2020-03-14 15:31:56 +0200

    Added the functionality to switchs screen when authenticated

--- noneTokenStack/none.js
+++ noneTokenStack/none.js
@@ -5,6 +5,7 @@ import Register from '../components/Register/Register';
 import Loading from '../components/Loading/Loading';
 import Home from '../components/Home/Home';
 const None = createStackNavigator();
+import {AsyncStorage} from 'react-native';
 
 //stack navigator for tackling user authentication
 const none = () => {
@@ -12,23 +13,23 @@ const none = () => {
   const authContext = useContext(AuthContext);
 
   //check if the user has been loaded from asyncstorage
-  const {user, loading} = authContext;
+  const {user, loading, getUser, isAuthenticated} = authContext;
 
   useEffect(() => {
      //get the token if there is any and check it
      //if it's authorized, switch to home screen,
      //else, switch to login screen again
-     
+     getUser();
   }, [])
   
     return (
         <None.Navigator>
         {/* if not, return a screen which prompts him to input his credentials */}
-        {user == null ? (
+        {isAuthenticated == false ? (
           <None.Screen name="Register" component={Register} options={{headerShown:false}}/>
         ) : (
         // if yes, return the home screen component
-          <None.Screen name="Home" component={HomeScreen} />
+          <None.Screen name="Home" component={Home}/>
         )}
         </None.Navigator>
     )
