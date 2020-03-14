6a23228eaa4b630902da94ce3e8d626201417d58
Tonislav Tachev <tachevv_@abv.bg>
2020-03-14 15:32:30 +0200

    Added the Home screen component

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,15 +1,14 @@
-import React, {useContext} from 'react'
+import React, {useContext, useEffect} from 'react'
 import {View, StyleSheet, Text} from 'react-native'
 import AuthContext from '../../context/AuthContext/authContext';
-const Home = () => {
+import Progressbar from '../Progress/Progressbar';
+const Home = ({route, navigation}) => {
 
   const authContext = useContext(AuthContext);
-  const {user} = authContext;
-
+  const {user, getUser, isAuthenticated} = authContext;
 
     return (
         <View>
-            
         </View>
     )
 }
