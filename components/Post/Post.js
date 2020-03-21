8ced0ca1fa7298caf08b033ef02060006ec6614a
Tonislav Tachev <tachevv_@abv.bg>
2020-03-21 20:39:10 +0200

    Displaying to post data

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, {useEffect, useContext} from 'react'
 import {View, Flatlist, StyleSheet, Text, SafeAreaView} from 'react-native'
-const Post = () => {
+import AuthContext from '../../context/AuthContext/authContext';
+import Progressbar from '../Progress/Progressbar';
+import PostItem from './PostItem';
+const Post = props => {
+    
+    const {user} = props
+    const authContext = useContext(AuthContext);
+
+    if(user === null){
+        return <Progressbar/>
+    }
+
     return (
         <View>
-            
+            {user.posts.map(el => <PostItem key={el._id} post={el} user={user}/>)}
         </View>
     )
 }
--- components/Post/PostItem.js
+++ components/Post/PostItem.js
@@ -1,11 +1,42 @@
 import React from 'react'
+import {TouchableOpacity, View, StyleSheet, Text, Image} from 'react-native'
+import EvilIcons from 'react-native-vector-icons/EvilIcons'
+import Entypo from 'react-native-vector-icons/Entypo'
+import SimpleLineIcons from 'react-native-vector-icons/SimpleLineIcons'
 
-const PostItem = () => {
+const PostItem = ({post, user}) => {
     return (
-        <div>
-            
-        </div>
+        <View style={styles.container}>
+            <View>
+                <Image  source={{uri:'https://htmlcolorcodes.com/assets/images/html-color-codes-color-tutorials-hero-00e10b1f.jpg'}} style={styles.img}/>
+            </View>
+
+            <View>
+               <View>
+                 <Text>
+                   {post.base}
+                 </Text>
+               </View>
+            </View>
+        </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container:{
+        flexDirection:'row',
+        width:'98%',
+        borderWidth:0.2,
+        marginTop:5,
+        backgroundColor:'#F7F9F9',
+        elevation:5,
+        padding:10
+    },
+    img:{
+        height:50,
+        width:50,
+        borderRadius:30
+    }
+})
 
 export default PostItem
